ffe34a7d236059583de0e73457ffc8da1de9b6dd
aung min <robbinakm12@hotmail.com>
2021-10-16 21:31:33 +0900

    added new feature , referencing the previous cell

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -13,20 +13,55 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { update_cell, bundle } = useAction();
   const bundleResult = useTypedSelector((state) => state.bundles[cell.id]);
+
+  const combinedCodes = useTypedSelector((state) => {
+    const { order, data } = state.cell;
+    const cells = order.map((id) => data[id]);
+    const combinedCodes = [
+      `
+      import _React from 'react';
+      import _ReactDOM from 'react-dom';
+
+      const root =document.querySelector("#root")
+      const show=(value)=>{
+
+        if (typeof value ==='object'){
+          if(value.$$typeof && value.props){
+            _ReactDOM.render(value,root)
+          }else{
+            root.innerHTML=JSON.stringify(value);
+          }
+        }else{
+          root.innerHTML=value
+        }
+      };
+      `,
+    ];
+    for (let c of cells) {
+      if (c.type === "code") {
+        combinedCodes.push(c.content);
+      }
+      if (c.type === cell.id) {
+        break;
+      }
+    }
+    return combinedCodes;
+  });
+
   useEffect(() => {
     if (!bundleResult) {
-      bundle(cell.id, cell.content);
+      bundle(cell.id, combinedCodes.join("\n"));
       return;
     }
 
     var timer = setTimeout(() => {
-      bundle(cell.id, cell.content);
+      bundle(cell.id, combinedCodes.join("\n"));
     }, 1000);
     return () => {
       clearTimeout(timer);
     };
     //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.id, cell.content, bundle]);
+  }, [cell.id, combinedCodes.join("\n"), bundle]);
 
   return (
     <Resizable direction="verticle">
